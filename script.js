66a0dc6e318ce71973d99aa8dc7893fef349fa6e
Liran Zuk <jozuk97@hotmail.co.il>
2021-08-07 10:28:05 +0300

    Finish task

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    var output1 = document.getElementById("results1");
+    var output2 = document.getElementById("results2");
+    var output3 = document.getElementById("results3");
+    var output4 = document.getElementById("results4");
+
+    //ans = Math.round(p * Math.pow(1+(r/100) , years))
+    ans = Math.round(p * (r/100)) * parseInt(years)
+    year = 2021 + parseInt(years)
+
+    output1.innerHTML = "If you deposit " + p + ",";
+    output2.innerHTML = "at an interest rate of " + r + "%.";
+    output3.innerHTML = "You will receive an amount of " + ans + ",";
+    output4.innerHTML = "in the year of " + year + ".";
+
     
 }
         
+function updateTextInput(val) {
+    document.getElementById('current_rate').value=val; 
+  }
+
+
